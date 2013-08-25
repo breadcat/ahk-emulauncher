514a9c6007c447fed6435cf652b3c8298dda9efb
breadcat <sendforththegunboats@gmail.com>
2013-08-26 00:11:16 +0100

    Add compile to symlink creation

--- symlinks.bat
+++ symlinks.bat
@@ -1,34 +1,57 @@
 rem todo:
-rem verify windows 7 and file paths
 rem create efficient way of looping for all commands
 rem batch compile ahk scripts
 
-set base=D:\games\emu
-set launchers=%userprofile%\Documents\GitHub\ahk-emulauncher\
+echo off
+cls
 
+set base=D:\games\emu
+set launchers=%userprofile%\Dropbox\git\ahk-emulauncher\
+set compiler="%programfiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
+
+if not exist %base% echo %base% not found, exiting.
+if not exist %base% pause
+if not exist %base% exit
 
 set emu=desmume
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=dolphin
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=dosbox
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=mednafen
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=nulldc
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=pcsx2
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=ppsspp
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=project64
-if exist "%base%\%emu%\launcher.ahk" del "%base%\%emu%\launcher.ahk"&&mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
+if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+%compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 pause
+exit
