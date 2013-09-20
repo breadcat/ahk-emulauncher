d817ee3d603597e5d57d2b16c4ff86f29e4e566d
breadcat <sendforththegunboats@gmail.com>
2013-09-20 22:38:36 +0100

    Symlink saves to dropbox
    revisions + savegames = pretty cool

--- symlinks.bat
+++ symlinks.bat
@@ -1,64 +1,91 @@
-rem todo:
-rem create efficient way of looping for all commands
-rem symlink emulator save directories to dropbox
+rem Emulator Launcher Compiler and Symlink Creator
+rem Requires Windows 7 mklink utility and a *very* specific folder structure
+rem Will also mercilessly delete existing save data, use with care
 
+rem todo:
+rem dosbox, nulldc, yabause savegame symlinks
+
+rem Environment
 echo off
 cls
 
+rem Variables
 set base=D:\games\emu
-set launchers=%userprofile%\Dropbox\git\ahk-emulauncher\
+set launchers=%userprofile%\Dropbox\git\ahk-emulauncher
 set compiler="%programfiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
+set savedir=%userprofile%\Dropbox\saves\emulators
 
+rem Preliminary check to prevent a trainload of problems
 if not exist %base% echo %base% not found, exiting.
 if not exist %base% pause
 if not exist %base% exit
 
 set emu=desmume
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
+rem Save game symlink
+if exist "%base%\%emu%\Battery" rd /s /q "%base%\%emu%\Battery"
+mklink /D "%base%\%emu%\Battery" "%savedir%\%emu%\"
 
 set emu=dolphin
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
+rem Save game symlink
+if exist "%base%\%emu%\User\GC" rd /s /q "%base%\%emu%\User\GC"
+if exist "%base%\%emu%\User\Wii" rd /s /q "%base%\%emu%\User\Wii"
+mklink /D "%base%\%emu%\User\GC" "%savedir%\%emu%-gc"
+mklink /D "%base%\%emu%\User\Wii" "%savedir%\%emu%-wii"
 
 set emu=dosbox
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=mednafen
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
+rem Save game symlink
+if exist "%base%\%emu%\sav" rd /s /q "%base%\%emu%\sav"
+mklink /D "%base%\%emu%\sav" "%savedir%\%emu%"
 
 set emu=nulldc
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 set emu=pcsx2
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
+rem Save game symlink
+if exist "%base%\%emu%\memcards" rd /s /q "%base%\%emu%\memcards"
+mklink /D "%base%\%emu%\memcards" "%savedir%\%emu%\"
 
 set emu=ppsspp
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
-rem PSP DLC Symlink
+rem DLC symlink
 if exist "%base%\%emu%\memstick\PSP\GAME" rd /s /q "%base%\%emu%\memstick\PSP\GAME%
 mklink /D "%base%\%emu%\memstick\PSP\GAME" "%base%\..\psp\dlc"
+rem Save game symlink
+if exist "%base%\%emu%\memstick\PSP\SAVEDATA" rd /s /q "%base%\%emu%\memstick\PSP\SAVEDATA"
+mklink /D "%base%\%emu%\memstick\PSP\SAVEDATA" "%savedir%\%emu%"
 
 set emu=project64
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
+rem Save game symlink
+if exist "%base%\%emu%\Save" rd /s /q "%base%\%emu%\Save"
+mklink /D "%base%\%emu%\Save" "%savedir%\%emu%"
 
 set emu=yabause
 if exist "%base%\%emu%\launcher.*" del "%base%\%emu%\launcher.*"
-mklink "%base%\%emu%\launcher.ahk" "%launchers%%emu%.ahk"
+mklink "%base%\%emu%\launcher.ahk" "%launchers%\%emu%.ahk"
 %compiler% /in "%emu%.ahk" /out "%base%\%emu%\launcher.exe"
 
 pause
